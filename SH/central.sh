b83ca1a5b21d4bf30c65fbc018f1a7892ea9770b
andermm <andersonmaliszewski@gmail.com>
2019-11-13 19:09:35 -0300

    draco

--- SH/central.sh
+++ SH/central.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 salloc -p draco --exclusive --nodelist=draco2,draco3,draco4,draco5 -J JOB -t 72:00:00
 
-ssh -n -f hype2 "sh -c 'cd $HOME/CMP223/SH; nohup ./nas_charac.sh > /dev/null 2>&1 &'"
+ssh -n -f draco2 "sh -c 'cd $HOME/CMP223/SH; nohup ./nas_charac.sh > /dev/null 2>&1 &'"
