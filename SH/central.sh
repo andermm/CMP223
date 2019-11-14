ace99b6e61f9f7879fd3a93bc07f2f5c0274b511
andermm <andersonmaliszewski@gmail.com>
2019-11-14 11:00:33 -0300

    update

--- SH/central.sh
+++ SH/central.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-salloc -p hype --exclusive --nodelist=hype1,hype2,hype3,hype4 -J JOB -t 72:00:00
+salloc -p hype --exclusive --nodelist=hype1,hype2,hype4,hype5 -J JOB -t 72:00:00
 
 ssh -n -f hype1 "sh -c 'cd $HOME/CMP223/SH; nohup ./nas_charac.sh > /dev/null 2>&1 &'"
