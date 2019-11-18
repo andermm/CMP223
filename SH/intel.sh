07a8e74e47152e04c3db4e2cdeca3f2cdcb04967
Anderson <andersonmaliszewski@gmail.com>
2019-11-18 14:21:19 -0300

    update

--- SH/intel.sh
+++ SH/intel.sh
@@ -13,6 +13,7 @@ IMB_SOURCE=$IMB/src_cpp/Makefile
 APP_BIN_IMB=$IMB/IMB-MPI1
 APP_TEST=PingPong
 PROCS=2
+
 #Other Variables
 START=`date +"%d-%m-%Y.%Hh%Mm%Ss"`
 OUTPUT_IMB_EXEC=$LOGS/IMB.$START.csv
@@ -21,29 +22,25 @@ OUTPUT_IMB_EXEC=$LOGS/IMB.$START.csv
 #################################Intel MPI Benchmarks#############################################
 cd $BENCHMARKS
 git clone --recursive https://github.com/intel/mpi-benchmarks.git
-cd $IMB
 sed -i 's,mpiicc,mpicc,g' $IMB_SOURCE
 sed -i 's,mpiicpc,mpicxx,g' $IMB_SOURCE
-make IMB-MPI1
+cd $IMB; make IMB-MPI1
 
 #Define the machine file and experimental project
-echo -e "draco2\ndraco3" > $LOGS/imb_nodes
-MACHINEFILE=$LOGS/imb_nodes
+echo -e "draco2\ndraco3" > $LOGS/nodes_imb
+MACHINEFILE=$LOGS/nodes_imb
+PROJECT=$R/intel_mpi_experimental_project_exec.csv
 for i in `seq 30`; do shuf -e IMB-MPI1,ib IMB-MPI1,eth IMB-MPI1,ipoib >> $PROJECT ; done
 sed -i '1s/^/apps,interface\n/' $PROJECT
-PROJECT=$R/intel_mpi_experimental_project_exec.csv
-
-
-for i in {1..10}; do mpiexec --mca btl self,tcp --mca btl_tcp_if_include eno2 -np 2 -machinefile nodes IMB-MPI1 PingPong; done
 
 #Read the experimental project
 tail -n +2 $PROJECT |
-while IFS=\; read -r apps interface
+while IFS=, read -r apps interface
 do
 
 #Clean the values
-	export apps=$(echo $apps | sed "s/\"//g")
-	export interface=$(echo $interface | sed "s/\"//g")
+	export apps=$(echo $apps)
+	export interface=$(echo $interface)
 
 #Define a single key
 	KEY="$apps-$interface"
@@ -63,7 +60,7 @@ do
 	fi
 	
 #Save the output according to the app
-	runline+="$BENCHMARKS/APP_BIN_IMB $TEST "
+	runline+="$BENCHMARKS/$APP_BIN_IMB $APP_TEST "
 	runline+="2>> $LOGS/errors_imb_exec "
 	runline+="&> >(tee -a $LOGS/BACKUP/$apps.$interface.exec.log > /tmp/intel_mb.out)"
 	
@@ -73,16 +70,20 @@ do
 	eval "$runline < /dev/null"
 
 #Save the results
-		for (( i = 0; i < 22; i++ )); do
-			echo "$apps,$interface" >> /tmp/intel_mb_tmp.out 
-		done
-	BYTES=`awk {'print $1'} /tmp/intel_mb.out | tail -n +35 | head -n -5`
-	TIME=`awk {'print $3'} /tmp/intel_mb.out | tail -n +35 | head -n -5`
-	Mbytes=`awk {'print $4'} /tmp/intel_mb.out | tail -n +35 | head -n -5`
-	paste -d"," /tmp/intel_mb_tmp.out $BYTES $TIME $Mbytes >> $OUTPUT_IMB
-	rm /tmp/imb_tmp.out	
-	echo "Done!"
+N=`tail -n +35 /tmp/intel_mb.out | awk {'print $1'} | grep -v '[^ 0.0-9.0]' | sed '/^[[:space:]]*$/d' | wc -l`
+
+	for (( i = 0; i < $N; i++ )); do
+		echo "$apps,$interface" >> /tmp/for.out
+	done
+	
+	tail -n +35 /tmp/intel_mb.out | awk {'print $1'} | grep -v '[^ 0.0-9.0]' | sed '/^[[:space:]]*$/d' > /tmp/BYTES
+    tail -n +35 /tmp/intel_mb.out | awk {'print $3'} | grep -v '[^ 0.0-9.0]' | sed '/^[[:space:]]*$/d' > /tmp/TIME
+    tail -n +35 /tmp/intel_mb.out | awk {'print $4'} | grep -v '[^ 0.0-9.0]' | sed '/^[[:space:]]*$/d' > /tmp/Mbytes
+    paste -d"," /tmp/for.out /tmp/BYTES /tmp/TIME /tmp/Mbytes >> $OUTPUT_IMB_EXEC
+    rm /tmp/for.out; rm /tmp/BYTES; rm /tmp/TIME; rm /tmp/Mbytes
+    
+    echo "Done!"
 
 done
-sed -i '1s/^/apps,interface,bytes,time,mbytes-sec\n/' $OUTPUT_IMB
+sed -i '1s/^/apps,interface,bytes,time,mbytes-sec\n/' $OUTPUT_IMB_EXEC
 exit
