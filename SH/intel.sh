b548dd38ab20c16fb98c88df440e07f001231bf9
Anderson <andersonmaliszewski@gmail.com>
2019-11-18 12:34:42 -0300

    update

--- SH/intel.sh
+++ SH/intel.sh
@@ -1,37 +1,88 @@
 #!/bin/bash
 
-BASE=$HOME/TESTE
+#Variable Directories
+BASE=$HOME/CMP223
 SCRIPTS=$BASE/SH
 BENCHMARKS=$BASE/BENCHMARKS
 LOGS=$BASE/LOGS
 R=$BASE/R
-INTEL=$BENCHMARKS/Intel/Executables/unix
-APP_BIN_ALYA=$ALYA/Alya.x
-APP_CONFIG_ALYA=$ALYA/config.in
-APP_TEST_CASE_A=
-APP_TEST_CASE_B=
+
+#Intel MPI Benchmarks Variables
+IMB=mpi-benchmarks
+IMB_SOURCE=$IMB/src_cpp/Makefile
+APP_BIN_IMB=$IMB/IMB-MPI1
+APP_TEST=PingPong
+PROCS=2
+#Other Variables
 START=`date +"%d-%m-%Y.%Hh%Mm%Ss"`
-OUTPUT_ALYA_EXEC=$LOGS/alya_exec.$START.csv
-PARTITION=hype
+OUTPUT_IMB_EXEC=$LOGS/IMB.$START.csv
 
-mkdir -p $BENCHMARKS
-# Download and compile Alya and 2 Test Case
+
+#################################Intel MPI Benchmarks#############################################
 cd $BENCHMARKS
-
 git clone --recursive https://github.com/intel/mpi-benchmarks.git
+cd $IMB
+sed -i 's,mpiicc,mpicc,g' $IMB_SOURCE
+sed -i 's,mpiicpc,mpicxx,g' $IMB_SOURCE
+make IMB-MPI1
+
+#Define the machine file and experimental project
+echo -e "draco2\ndraco3" > $LOGS/imb_nodes
+MACHINEFILE=$LOGS/imb_nodes
+for i in `seq 30`; do shuf -e IMB-MPI1,ib IMB-MPI1,eth IMB-MPI1,ipoib >> $PROJECT ; done
+sed -i '1s/^/apps,interface\n/' $PROJECT
+PROJECT=$R/intel_mpi_experimental_project_exec.csv
 
 
-wget -c https://repository.prace-ri.eu/ueabs/ALYA/2.1/Alya.tar.gz;tar -zxf Alya.tar.gz;rm -rf Alya.tar.gz
-cd $ALYA; wget -c https://repository.prace-ri.eu/ueabs/ALYA/2.1/TestCaseA.tar.gz;tar -zxf TestCaseA.tar.gz;rm -rf TestCaseA.tar.gz
-cd $ALYA; wget -c https://repository.prace-ri.eu/ueabs/ALYA/2.1/TestCaseB.tar.gz;tar -zxf TestCaseB.tar.gz;rm -rf TestCaseB.tar.gz
+for i in {1..10}; do mpiexec --mca btl self,tcp --mca btl_tcp_if_include eno2 -np 2 -machinefile nodes IMB-MPI1 PingPong; done
 
-cd $ALYA; cp configure.in/config_gfortran.in config.in
-sed -i 's,mpif90,mpifort,g' $APP_CONFIG_ALYA
-./configure -x nastin parall
+#Read the experimental project
+tail -n +2 $PROJECT |
+while IFS=\; read -r apps interface
+do
 
-##Compile Alya
-cd $ALYA; make metis4;make;cd $BASE
+#Clean the values
+	export apps=$(echo $apps | sed "s/\"//g")
+	export interface=$(echo $interface | sed "s/\"//g")
 
-mkdir -p $BASE/LOGS/BACKUP
+#Define a single key
+	KEY="$apps-$interface"
+	echo $KEY
 
+#Prepare the command for execution
+	runline=""
+	runline+="mpiexec -np $PROCS -machinefile $MACHINEFILE --mca btl self,"
+
+#Select interface
+	if [[ $interface == ib ]]; then
+		runline+="openib --mca btl_openib_if_include mlx5_0:1 "	
+	elif [[ $interface == ipoib ]]; then
+		runline+="tcp --mca btl_tcp_if_include ib0 "
+	else
+		runline+="tcp --mca btl_tcp_if_include eno2 "
+	fi
+	
+#Save the output according to the app
+	runline+="$BENCHMARKS/APP_BIN_IMB $TEST "
+	runline+="2>> $LOGS/errors_imb_exec "
+	runline+="&> >(tee -a $LOGS/BACKUP/$apps.$interface.exec.log > /tmp/intel_mb.out)"
+	
+
+#Execute the experiments
+	echo "Executing >> $runline <<"
+	eval "$runline < /dev/null"
+
+#Save the results
+		for (( i = 0; i < 22; i++ )); do
+			echo "$apps,$interface" >> /tmp/intel_mb_tmp.out 
+		done
+	BYTES=`awk {'print $1'} /tmp/intel_mb.out | tail -n +35 | head -n -5`
+	TIME=`awk {'print $3'} /tmp/intel_mb.out | tail -n +35 | head -n -5`
+	Mbytes=`awk {'print $4'} /tmp/intel_mb.out | tail -n +35 | head -n -5`
+	paste -d"," /tmp/intel_mb_tmp.out $BYTES $TIME $Mbytes >> $OUTPUT_IMB
+	rm /tmp/imb_tmp.out	
+	echo "Done!"
+
+done
+sed -i '1s/^/apps,interface,bytes,time,mbytes-sec\n/' $OUTPUT_IMB
 exit
