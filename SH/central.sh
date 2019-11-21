9313a4abf0896497acaff375799dd1e6ad3c8162
andermm <andersonmaliszewski@gmail.com>
2019-11-21 09:08:00 -0300

    update

--- SH/central.sh
+++ SH/central.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 BASE=$HOME/CMP223
-salloc -p hype --exclusive --nodelist=hype1,hype2,hype4,hype5 -J JOB -t 72:00:00
-ssh -n -f hype1 "sh -c 'cd $HOME/CMP223; nohup ./SH/benchmarks_exec.sh > $BASE/LOGS/scriptlog 2>&1 &'"
+salloc -p hype --exclusive --nodelist=hype2,hype3,hype4,hype5 -J JOB -t 72:00:00
+ssh -n -f hype2 "sh -c 'cd $HOME/CMP223; nohup ./SH/experiments_exec.shh > $BASE/LOGS/apps_std_out.log 2>&1 &'"
