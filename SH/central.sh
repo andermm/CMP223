73755deaa9f7c79b5008db43f240274010f9ddf8
andermm <andersonmaliszewski@gmail.com>
2019-11-21 14:01:23 -0300

    upadte

--- SH/central.sh
+++ SH/central.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 BASE=$HOME/CMP223
-LOGS=$BASE/LOGS
-mkdir $LOGS
 salloc -p hype --exclusive --nodelist=hype2,hype3,hype4,hype5 -J JOB -t 72:00:00
-ssh -n -f hype2 "sh -c 'cd $HOME/CMP223; nohup ./SH/experiments_exec.sh > $BASE/$LOGS/exec_script_std_out-err.log 2>&1 &'"
+ssh -n -f hype2 "sh -c 'cd $HOME/CMP223; nohup ./SH/experiments_exec.sh > $BASE/apps_std_out.log 2>&1 &'"
